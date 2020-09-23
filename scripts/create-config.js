e796398d17e30e263c4ee6538058c508c7160069
Sami Sayegh <ssayegh@coveo.com>
2020-09-23 12:17:58 -0400

    prevent writing file if it already exists

--- scripts/create-config.js
+++ scripts/create-config.js
@@ -1,7 +1,11 @@
-const {writeFileSync} = require('fs');
+const {writeFileSync, existsSync} = require('fs');
 const {resolve} = require('path');
 
 const filePath = resolve(__dirname, '../config.json');
+
+if (existsSync(filePath)) {
+  return;
+}
 
 const config = {
   organizationId: '',
@@ -9,5 +13,6 @@ const config = {
   pageName: '',
   apiKey: ''
 }
+
 
 writeFileSync(filePath, JSON.stringify(config, undefined, 2))
